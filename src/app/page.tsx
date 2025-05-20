35c2f0f2fa25378b16e350bb6bf15665876dc36b
mathanta <ethan@mattafam.com>
2025-05-20 17:28:13 -0600

    Made a form to send an email. Fixed the title on both desktop and mobile. Fixed a lot of spacing issues, like unwanted horizontal scroll.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,77 @@
-import Image from 'next/image'
+'use client'
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
 
 export default function Main() {
+  const [status, setStatus] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setStatus('Sending...');
+
+    try {
+      await emailjs.sendForm(
+        'service_ozjqe3c',
+        'template_fnnkmzx',
+        e.currentTarget,
+        '-8abfnUvU4fDxuG4z'
+      );
+      setStatus('Something went wrong. Please try again.');
+      e.currentTarget.reset();
+      setTimeout(() => setStatus(''), 2000);
+    } catch {
+      setStatus('Message sent! Thank you.');
+      setTimeout(() => setStatus(''), 2000);
+    }
+  };
+
   return (
     <>
-      <Image className='portrait' src="portrait.svg" alt='placeholder' height={250} width={250} />
-      <div className='title-header'>
-        <h1>
-          MiMi&apos;s
-        </h1>
-        <h2>
-          WORKSHOP
-        </h2>
-        <h3>
-          Hand-Made Crafts
-        </h3>
+      {status && (
+        <div className="status-overlay">
+          <div className="status-message">
+            {status}
+          </div>
+        </div>
+      )}
+
+      <div className="contact-form-container">
+        <form onSubmit={handleSubmit}>
+          <div className='form-group' id='form-title'>
+            <p>Send a Message to MiMi</p>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Name"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="email"
+              name="user_email"
+              placeholder="Email"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <textarea
+              name="message"
+              placeholder='Custom order, question, etc...'
+              required
+              rows={4}
+            />
+          </div>
+
+          <button id='submit-button' type="submit">
+            Send
+          </button>
+        </form>
       </div>
     </>
   );
