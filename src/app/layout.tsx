0df6138a4902cf951e95320021d8b819d8c9d09a
mathanta <ethan@mattafam.com>
2025-05-31 01:27:30 -0600

    Switched from imported Google fonts to local woff files.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,8 @@
 import type { Metadata } from "next";
-import { Pacifico, Caveat_Brush } from "next/font/google";
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 import "./globals.css";
-import Footer from '../../components/Footer'
-
-const pacifico = Pacifico({
-  weight: ['400'],
-  subsets: ['latin'],
-  variable: '--font-pacifico'
-});
-
-const caveatBrush = Caveat_Brush({
-  weight: ['400'],
-  subsets: ['latin'],
-  variable: '--font-caveat-brush'
-});
+import Footer from "../../components/Footer";
 
 export const metadata: Metadata = {
   title: "MiMi's Workshop",
@@ -29,25 +16,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${caveatBrush.variable} ${pacifico.variable} antialiased`}>
+      <body>
         <div className="background-layers"></div>
         <div className="ribbon">Under Construction</div>
         <div className="portrait-container">
           <Link href="/about">
-            <Image className='portrait' src="portrait.svg" alt='placeholder' height={250} width={250} />
+            <Image
+              className="portrait"
+              src="portrait.svg"
+              alt="placeholder"
+              height={250}
+              width={250}
+            />
           </Link>
         </div>
         <Link href="/">
-          <div className='title-header'>
-            <h1>
-              MiMi&apos;s
-            </h1>
-            <h2>
-              WORKSHOP
-            </h2>
-            <h3>
-              Hand-Made Crafts
-            </h3>
+          <div className="title-header">
+            <h1>MiMi&apos;s</h1>
+            <h2>WORKSHOP</h2>
+            <h3>Hand-Made Crafts</h3>
           </div>
         </Link>
         {children}
